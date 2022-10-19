4c1762a6b8d0c2aac54c67b3f3c6e638a913de78
ActuallyGiggles <m.alphonse.r.k@gmail.com>
2022-10-19 11:15:57 -0700

    memory usage in stats

--- stats/stats.go
+++ stats/stats.go
@@ -2,12 +2,14 @@ package stats
 
 import (
 	"markov-generator/markov"
+	"runtime"
 	"time"
 )
 
 type Stats struct {
 	StartTime      time.Time
 	RunTime        time.Duration
+	MemoryUsage    MemoryUsage
 	WriteMode      string
 	TimeUntilWrite time.Duration
 	CurrentCount   int
@@ -35,4 +37,29 @@ func GetStats() (stats Stats) {
 	stats.PeakIntake = markov.PeakIntake()
 
 	return stats
+}
+
+type MemoryUsage struct {
+	Allocated      uint64
+	TotalAllocated uint64
+	System         uint64
+	NumGC          uint32
+}
+
+// PrintMemUsage outputs the current, total and OS memory being used. As well as the number
+// of garage collection cycles completed.
+func PrintMemUsage() (mu MemoryUsage) {
+	var m runtime.MemStats
+	runtime.ReadMemStats(&m)
+	// For info on each, see: https://golang.org/pkg/runtime/#MemStats
+	mu.Allocated = bToMb(m.Alloc)
+	mu.TotalAllocated = bToMb(m.TotalAlloc)
+	mu.System = bToMb(m.Sys)
+	mu.NumGC = m.NumGC
+
+	return mu
+}
+
+func bToMb(b uint64) uint64 {
+	return b / 1024 / 1024
 }
